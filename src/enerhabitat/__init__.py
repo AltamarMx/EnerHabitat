c571eb97270db7d008c7ad7992f9a345bfb29238
Fernando Rodríguez <ferrodriguez2509@gmail.com>
2025-05-04 20:16:33 -0600

    refactor:documentacion calculateTSA

--- src/enerhabitat/__init__.py
+++ src/enerhabitat/__init__.py
@@ -1,47 +1,53 @@
 import pvlib
 import pytz
 
+from datetime import datetime
 from .ehtools import *
 
-def calculateTsa(epw_file_path:str, convection_heat_transfer:float, solar_absortance:float, inclination:float, azimuth:float, month:str, year:str):
+def calculateTsa(epw_file_path: str,
+    solar_absortance: float,
+    surface_tilt: float,
+    surface_azimuth: float,
+    convection_heat_transfer: float = 13,
+    day: str = "15",
+    month: str = "current_month",
+    year: str = "current_year",
+) -> pd.DataFrame:
     """
-    Calcula la temperatura sol/aire por segundo para el día promedio que experimenta una superficie basado en datos de un archivo EPW 
+       Calculates the sun-air temperature per second for the average day experienced by a surface based on EPW file data.
 
     Args:
-        epw_file_path (str): Ruta del archivo EPW
-        convection_heat_transfer (float): Indice de transferencia de calor por convección del sistema (eg. 13)
-        solar_absortance (float): Absortancia del material externo del sistema
-        inclination (float): Inclinación de la superficie con respecto al suelo (90° == Vertical)
-        azimuth (float): Desviacion con respecto al norte (0° == Norte)
-        month (str): Mes de interés
-        year (str): Año de interés
-    
+        epw_file_path (str): Path to the EPW file.
+        solar_absortance (float): Solar absorptance of the system's external material.
+        surface_tilt (float): Surface tilt relative to the ground (90° == Vertical).
+        surface_azimuth (float): Deviation from true north (0° == North).
+        convection_heat_transfer (float): Convective heat transfer coefficient of the system (e.g., 13).
+        day (str, optional): Day of interest. Defaults to "15".
+        month (str, optional): Month of interest. Defaults to "current_month".
+        year (str, optional): Year of interest. Defaults to "current_year".
+
     Returns:
-        DataFrame : Predicción de la TSA por segundo para el día promedio del mes y año de interés
+        pd.DataFrame: Predicted sun-air temperature (TSA) per second for the average day of the specified month and year.
     """
     
-    epw, latitud, longitud, altitud, timezone = readEPW(epw_file_path,year,alias=True,warns=False)
-    timezone=pytz.timezone('Etc/GMT'+f'{(-timezone):+}')
-    
-    
-    dia = '15'
-    mes = month
-    año = year
-    absortancia = solar_absortance
-    h = convection_heat_transfer
+    if month == "current_month": month = datetime.now().month
+    if year == "current_year": year = datetime.now().year
 
     # Parámetros de la superficie
-    surface_tilt = inclination  # 90 - Vertical
-    surface_azimuth = azimuth  # 270
+    # tilt = 90 = Vertical
+    # azimuth = 270
 
     if surface_tilt == 0:
         LWR = 3.9
     else:
         LWR = 0.
 
-    f1 = f'{año}-{mes}-{dia} 00:00'
-    f2 = f'{año}-{mes}-{dia} 23:59'
-
+    f1 = f'{year}-{month}-{day} 00:00'
+    f2 = f'{year}-{month}-{day} 23:59'
+    
+    epw, latitud, longitud, altitud, timezone = readEPW(epw_file_path,year,alias=True,warns=False)
+    timezone=pytz.timezone('Etc/GMT'+f'{(-timezone):+}')
+    
     dia = pd.date_range(start=f1, end=f2, freq='1s',tz=timezone)
     location = pvlib.location.Location(latitude = latitud, 
                                        longitude=longitud, 
@@ -53,13 +59,13 @@ def calculateTsa(epw_file_path:str, convection_heat_transfer:float, solar_absort
     del dia['apparent_elevation']
     
     sunrise,_ = get_sunrise_sunset_times(dia)
-    tTmax,Tmin,Tmax = calculate_tTmaxTminTmax(mes,epw)
+    tTmax,Tmin,Tmax = calculate_tTmaxTminTmax(month,epw)
 
     # Calcular la temperatura ambiente y agregarla al DataFrame
     dia = temperature_model(dia, Tmin, Tmax, sunrise, tTmax)
     
     # Agrega Ig, Ib, Id a dia 
-    dia = add_IgIbId_Tn(dia,epw,mes,f1,f2,timezone)
+    dia = add_IgIbId_Tn(dia,epw,month,f1,f2,timezone)
     
     total_irradiance = pvlib.irradiance.get_total_irradiance(
         surface_tilt=surface_tilt,
@@ -73,12 +79,12 @@ def calculateTsa(epw_file_path:str, convection_heat_transfer:float, solar_absort
     dia['Is'] = total_irradiance.poa_global
     
     # Agrega Tsa, DeltaTn
-    dia['Tsa'] = dia.Ta + dia.Is*absortancia/h - LWR
+    dia['Tsa'] = dia.Ta + dia.Is*solar_absortance/convection_heat_transfer - LWR
     DeltaTa= dia.Ta.max() - dia.Ta.min()
 
     dia['DeltaTn'] = calculate_DtaTn(DeltaTa)
     
-    epw_mes = epw.loc[epw.index.month==int(mes)]
+    epw_mes = epw.loc[epw.index.month==int(month)]
     hora_minutos = epw_mes.resample('D').To.idxmax()
     hora = hora_minutos.dt.hour
     minuto = hora_minutos.dt.minute
